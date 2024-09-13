2a4324e1226071d688a2ab63f60db0d1b1ff6c30
MisterCore <Heiko-YouTube@web.de>
2024-09-13 17:35:11 +0200

    Updated build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    id("java-library")
-    id("net.labymod.gradle")
-    id("net.labymod.gradle.addon")
+    id("net.labymod.labygradle")
+    id("net.labymod.labygradle.addon")
 }
 
-group = "org.example"
-version = "1.1.0"
+val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
+val addonVersion = "1.4.7"
+
+group = "net.terramc"
+version = providers.environmentVariable("VERSION").getOrElse(addonVersion)
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
@@ -21,85 +23,32 @@ tasks.withType<Javadoc>{
 
 labyMod {
     defaultPackageName = "net.terramc.addon" //change this to your main package name (used by all modules)
+
+    minecraft {
+        registerVersion(versions.toTypedArray()) {
+            runs {
+                getByName("client") {
+                    // When the property is set to true, you can log in with a Minecraft account
+                    // devLogin = true
+                }
+            }
+        }
+    }
+
     addonInfo {
         namespace = "terramc"
         displayName = "TerraMCnet"
         author = "MisterCore"
         description = "This addon offers you advanced information on TerraMC.net. For example it will show you your current nickname, game rank and much more."
         minecraftVersion = "*"
-        version = System.getenv().getOrDefault("VERSION", "1.4.7")
-    }
-
-    minecraft {
-        registerVersions(
-                "1.8.9",
-                "1.12.2",
-                "1.16.5",
-                "1.17.1",
-                "1.18.2",
-                "1.19.2",
-                "1.19.3",
-                "1.19.4",
-                "1.20.1",
-                "1.20.2",
-                "1.20.4",
-                "1.20.5",
-                "1.20.6",
-                "1.21",
-                "1.21.1"
-        ) { version, provider ->
-            configureRun(provider, version)
-        }
-
-        subprojects.forEach {
-            if (it.name != "game-runner") {
-                filter(it.name)
-            }
-        }
-    }
-
-    addonDev {
-        productionRelease()
+        version = System.getenv().getOrDefault("VERSION", addonVersion)
     }
 }
 
 subprojects {
-    plugins.apply("java-library")
-    plugins.apply("net.labymod.gradle")
-    plugins.apply("net.labymod.gradle.addon")
+    plugins.apply("net.labymod.labygradle")
+    plugins.apply("net.labymod.labygradle.addon")
 
-    repositories {
-        maven("https://libraries.minecraft.net/")
-        maven("https://repo.spongepowered.org/repository/maven-public/")
-    }
-}
-
-fun configureRun(provider: net.labymod.gradle.core.minecraft.provider.VersionProvider, gameVersion: String) {
-    provider.runConfiguration {
-        mainClass = "net.minecraft.launchwrapper.Launch"
-        jvmArgs("-Dnet.labymod.running-version=${gameVersion}")
-        jvmArgs("-Dmixin.debug=true")
-        jvmArgs("-Dnet.labymod.debugging.all=true")
-        jvmArgs("-Dmixin.env.disableRefMap=true")
-
-        args("--tweakClass", "net.labymod.core.loader.vanilla.launchwrapper.LabyModLaunchWrapperTweaker")
-        args("--labymod-dev-environment", "true")
-        args("--addon-dev-environment", "true")
-    }
-
-    provider.javaVersion = JavaVersion.VERSION_21
-
-    provider.mixin {
-        val mixinMinVersion = when (gameVersion) {
-            "1.8.9", "1.12.2", "1.16.5" -> {
-                "0.6.6"
-            }
-
-            else -> {
-                "0.8.2"
-            }
-        }
-
-        minVersion = mixinMinVersion
-    }
+    group = rootProject.group
+    version = rootProject.version
 }
